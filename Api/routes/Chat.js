661b51815717b7b72e3d3dd3839484460fd70a6c
bobardou <jeronimoevilar@outlook.com>
2023-06-13 15:42:15 -0300

    routes fix

--- Api/routes/Chat.js
+++ Api/routes/Chat.js
@@ -10,7 +10,7 @@ const {
 
 app.get("/:chatId", getChatInfo);
 
-app.get("/friend", getChatParticipantInfo);
+app.post("/friend", getChatParticipantInfo);
 
 app.get("/allchats/:userId", getAllUserChats);
 
